3a95a56cf1b9b8f3c0795547b442787a6eccdfac
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-06 15:39:09 +0600

    frartur

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -5,16 +5,29 @@ const Featured = () => {
   return (
     <div className="featured">
       <div className="container">
-        <div className="left"><h1>Find the right service right <span>plamber</span> & right away</h1>
-      <div className="search">
-      <div className="searchInput">
-        <img src="" alt="" />
-        <input type="text" placeholder='Try"TO building mobile app"'/>
-      </div>
-      </div>
+        <div className="left">
+          <h1>
+            Find the right service right <span>plamber</span> & right away
+          </h1>
+          <div className="search">
+            <div className="searchInput">
+              <img src="" alt="" />
+              <input type="text" placeholder='Try"TO building mobile app"' />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="popular">
+            <button>popular</button>
+            <button>plamber</button>
+            <button>plamber</button>
+            <button>plamber</button>
+            <button>plamber</button>
+          </div>
         </div>
-        <div className="right"></div>
+
+        <div className="right">
         <img src="" alt="" />
+        </div>
       </div>
     </div>
   );
