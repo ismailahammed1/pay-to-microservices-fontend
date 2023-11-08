e9a6c9621a4e56e72ca4cf23657bfef3f11febea
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-09 04:27:34 +0600

    data

---
+++ data.js
@@ -0,0 +1,75 @@
+
+export const card=[
+      {
+        id: 1,
+        name: "John Smith",
+        category: "Carpenter",
+        experience: "10 years",
+        location: "New York",
+        hourlyRate: "$25",
+        availability: "Full-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481604/Carpenter-1_wtzlxd.jpg"
+      },
+      {
+        id: 2,
+        name: "Mary Johnson",
+        category: "Housekeeper",
+        experience: "5 years",
+        location: "Los Angeles",
+        hourlyRate: "$20",
+        availability: "Part-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481659/download_ud1iod.jpg"
+      },
+      {
+        id: 3,
+        name: "David Brown",
+        category: "Gardener",
+        experience: "8 years",
+        location: "Chicago",
+        hourlyRate: "$30",
+        availability: "Full-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699482199/gardener-job-description-1659711272_cncxo9_mxx33o.webp"
+      },
+      {
+        id: 4,
+        name: "Sarah Davis",
+        category: "Nanny",
+        experience: "7 years",
+        location: "Houston",
+        hourlyRate: "$18",
+        availability: "Full-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481786/1028379220Nanny_gbbwth.webp"
+      },
+      {
+        id: 5,
+        name: "Michael Wilson",
+        category: "Plumber",
+        experience: "12 years",
+        location: "Miami",
+        hourlyRate: "$35",
+        availability: "Part-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481789/download_riruin.jpg"
+      },
+      {
+        id: 6,
+        name: "Linda Martinez",
+        category: "Elderly Caregiver",
+        experience: "6 years",
+        location: "San Francisco",
+        hourlyRate: "$22",
+        availability: "Full-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481843/senior-man-with-helpful-volunteer-in-the-garden-LMJAN27_yg3fzb.webp"
+      },
+      {
+        id: 7,
+        name: "Robert Taylor",
+        category: "Pet Sitter",
+        experience: "4 years",
+        location: "Seattle",
+        hourlyRate: "$15",
+        availability: "Part-time",
+        Image:"https://res.cloudinary.com/dfn1s2ysa/image/upload/v1699481519/girl-5_sx7bot.png"
+      }
+    ]
+
+  
