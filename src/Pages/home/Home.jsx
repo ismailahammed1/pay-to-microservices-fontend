53ecedf112da75c50485c478a1a8058c59d7133d
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-08 13:51:01 +0600

    trusted by

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Featured from '../../components/Featured/Featured'
+import TrustedBy from '../../components/TrustedBy/TrustedBy'
 import "./Home"
 const Home = () => {
   return (
     <div className='home'>
       <Featured/>
+      <TrustedBy/>
     </div>
   )
 }
---
+++ src/components/TrustedBy/TrustedBy.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./TrustedBy.scss"
+
+const TrustedBy = () => {
+  return (
+    <div className='trustedBy'>
+        <div className='container'>
+            <span>TrustedBy</span>
+            <img src="../../../public/logo/pngwing.com.png" alt="" />
+            <img src="../../../public/logo/pngwing.com (1).png" alt="" />
+            <img src="../../../public/logo/pngwing.com (2).png" alt="" />   
+            <img src="../../../public/logo/pngwing.com (3).png" alt="" />   
+            <img src="../../../public/logo/pngwing.com (4).png" alt="" />   
+            <img src="../../../public/logo/pngwing.com (5).png" alt="" />   
+        </div>
+    </div>
+  )
+}
+
+export default TrustedBy
