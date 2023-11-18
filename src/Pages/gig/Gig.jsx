abf9b85ccf2357203127c4fe33f5c0c5312d95e3
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-18 23:47:13 +0600

    gigs and gid design done

--- src/Pages/gig/Gig.jsx
+++ src/Pages/gig/Gig.jsx
@@ -1,6 +1,6 @@
 import { Slider } from "infinite-react-carousel";
 import React from "react";
-import "./Gig";
+import "./Gig.scss";
 const Gig = () => {
   return (
     <div className="gig">
@@ -36,7 +36,7 @@ const Gig = () => {
                 src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
                 alt=""
               />
-              <span>5</span>
+               <span>5.0</span>
             </div>
           </div>
           <Slider slideToShow={1} arrowsScroll={1} className="slider">
@@ -74,7 +74,7 @@ const Gig = () => {
               />
               <div className="info">
                 <span>Anna Bell</span>
-                <div className="star">
+                <div className="stars">
                   <img
                     src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
                     alt=""
@@ -95,7 +95,7 @@ const Gig = () => {
                     src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
                     alt=""
                   />
-                  <span>5</span>
+                   <span>5.0</span>
                 </div>
                 <button>Contact Me</button>
               </div>
@@ -112,7 +112,7 @@ const Gig = () => {
                 </div>
                 <div className="item">
                   <span className="title">Avg. Respose Time</span>
-                  <span className="desc">4 hour</span>
+                  <span className="desc">2 hour</span>
                 </div>
                 <div className="item">
                   <span className="title">done work</span>
@@ -120,13 +120,276 @@ const Gig = () => {
                 </div>
                 <div className="item">
                   <span className="title">Languages</span>
-                  <span className="desc">Bangal</span>
+                  <span className="desc">English</span>
                 </div>
               </div>
               <hr />
-              <p>I'm Jhone, an experienced car repair specialist. I diagnose and fix various car issues, from routine maintenance like oil changes and brake inspections to major repairs involving engines and transmissions. I excel in diagnosing problems, providing accurate estimates, and delivering quality repairs. I prioritize regular maintenance for optimal vehicle performance and longevity. Trust me for expert car care and reliable service at every step.</p>
+              <p>
+                {" "}
+                It seems like your request is incomplete. If you meant to ask
+                about something specific or would like assistance with sorting
+                information, please provide more details or clarify your
+                question. I'm here to help!r issues, from routine maintenance
+                like oil changes and brake inspections to major repairs
+                involving engines and transmissions. I excel in diagnosing
+                problems, providing accurate estimates, and delivering quality
+                repairs. I prioritize regular maintenance for optimal vehicle
+                performance and longevity. Trust me for expert car care and
+                reliable service at every step.
+              </p>
             </div>
           </div>
+          <div className="reviews">
+            <h2>Reviews</h2>
+            <div className="item">
+              <div className="user">
+                <img className="pp"
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700062034/images_xhicbw.jpg"
+                  alt=""
+                />
+                <div className="info">
+                  <span>Garner David</span>
+                  <div className="country">
+                    <img
+                      src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257157/pngtree-flowy-flag-of-palestine-with-splash-effect-png-image_6599848_pwd2yy.png"
+                      alt=""
+                    />
+                    <span>palestine</span>
+                  </div>
+                </div>
+              </div>
+              <div className="star">
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                 <span>5.0</span>
+              </div>
+              <p>
+                It seems like your request is incomplete. If you meant to ask
+                about something specific or would like assistance with sorting
+                information, please provide more details or clarify your
+                question. I'm here to help!
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257510/126473_xm0q4f.png"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257511/126504_hsvr1h.png"
+                  alt=""
+                />
+                <span>NO</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700061436/download_gknafp.jpg"
+                  alt=""
+                  className="pp"
+                />
+                <div className="info">
+                  <span>lyle giles</span>
+                  <div className="country">
+                    <img
+                      src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257157/pngtree-flowy-flag-of-palestine-with-splash-effect-png-image_6599848_pwd2yy.png"
+                      alt=""
+                    />
+                    <span>Palastine</span>
+                  </div>
+                </div>
+              </div>
+              <div className="star">
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                 <span>5.0</span>
+              </div>
+              <p>
+                Thrilled with the amazing car repair! The service was efficient,
+                and the technicians demonstrated impressive expertise. Highly
+                recommend their outstanding work!
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257510/126473_xm0q4f.png"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257511/126504_hsvr1h.png"
+                  alt=""
+                />
+                <span>NO</span>
+              </div>
+            </div>
+            <hr />
+            <div className="item">
+              <div className="user">
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700061436/download_gknafp.jpg"
+                  alt=""
+                  className="pp"
+                />
+                <div className="info">
+                  <span>lyle giles</span>
+                  <div className="country">
+                    <img
+                      src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257157/pngtree-flowy-flag-of-palestine-with-splash-effect-png-image_6599848_pwd2yy.png"
+                      alt=""
+                    />
+                    <span>Palastine</span>
+                  </div>
+                </div>
+              </div>
+              <div className="star">
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700063146/start_53876-25533_yhvo8x.avif"
+                  alt=""
+                />
+                 <span>5.0</span>
+              </div>
+              <p>
+                Thrilled with the amazing car repair! The service was efficient,
+                and the technicians demonstrated impressive expertise. Highly
+                recommend their outstanding work!
+              </p>
+              <div className="helpful">
+                <span>Helpful</span>
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257510/126473_xm0q4f.png"
+                  alt=""
+                />
+                <img
+                  src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700257511/126504_hsvr1h.png"
+                  alt=""
+                />
+                <span>NO</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="price">
+            <h3>Car repair services</h3>
+            <h2>$45.54</h2>
+          </div>
+
+          <p>i will service car any issues</p>
+          <div className="details">
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700259343/992700_xltvsx.png"
+                alt=""
+              />
+              <span>2 days service</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700263922/2089548_rqvoma.png"
+                alt=""
+              />
+              <span>Done And check</span>
+            </div>
+          </div>
+          <div className="features">
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>promt</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>Atwork Delivery</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>Atwork Delivery</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>Atwork Delivery</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>Atwork Delivery</span>
+            </div>
+            <div className="item">
+              <img
+                src="https://res.cloudinary.com/dfn1s2ysa/image/upload/v1700260719/190411_caccy2.png"
+                alt=""
+              />
+              <span>Addotional Service</span>
+            </div>
+          </div>
+          <button>Continue</button>
         </div>
       </div>
     </div>
