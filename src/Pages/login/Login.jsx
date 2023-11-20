0990f2aa7f022b466d28eb4490f1f8cd1d1e6ec7
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-20 23:58:58 +0600

    login

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import "./Loging.scss"
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <from>
+        <h1>Sign IN</h1>
+        <label htmlFor="">Username</label>
+        <input type="text" name='username' placeholder='Jhon doe'/>
+        <label htmlFor="">Password</label>
+        <input type="text" name="password" placeholder="johndoe@gmail.com" />
+        <button type='submit'>Login</button>
+      </from>
+    </div>
   )
 }
 
