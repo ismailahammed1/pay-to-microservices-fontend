453c1763665bf1fef7b1744a61020c177c53b554
ismailahammed1 <ismailahmmedroman@gmail.com>
2023-11-21 14:15:36 +0600

    login and axios set

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import "./Loging.scss"
+import axios from "axios";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Loging.scss";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8800/api/auth/login", {
+        username,
+        password
+      });
+console.log(res.data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  }
+
   return (
     <div className='login'>
-      <from>
-        <h1>Sign IN</h1>
-        <label htmlFor="">Username</label>
-        <input type="text" name='username' placeholder='Jhon doe'/>
-        <label htmlFor="">Password</label>
-        <input type="text" name="password" placeholder="johndoe@gmail.com" />
+      <form onSubmit={handleSubmit}>
+        <h1>Sign In</h1>
+        <label htmlFor="username">Username</label>
+        <input type="text" name='username' placeholder='John Doe' onChange={e => setUsername(e.target.value)} />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="johndoe@gmail.com" autoComplete="off" onChange={e => setPassword(e.target.value)} />
+        <Link className='forget' to="/">Forget password</Link>
         <button type='submit'>Login</button>
-      </from>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
